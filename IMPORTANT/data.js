7934ce0993de728a58d23980489c4c3221f9e23f
eeyyeeyy <eeyyeeyy0925@gmail.com>
2024-11-28 12:03:57 +0800

    IMPORTANT

---
+++ IMPORTANT/data.js
@@ -0,0 +1,359 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "大門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -0.17587684467361342,
+        "pitch": -0.21456974487236025,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3546427874801683,
+          "pitch": 0.1752268987746124,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.0490636460183804,
+          "pitch": 0.16919256489146584,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "仁德全興路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.487055282152805,
+        "pitch": -0.008749799259195612,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2531736267609475,
+          "pitch": 0.2810801232376754,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.8616780764934404,
+          "pitch": 0.4156753173403551,
+          "rotation": 6.283185307179586,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "公車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5325868923356403,
+        "pitch": -0.2912022371718628,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.052831814863578686,
+          "pitch": 0.18193610798616433,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -3.029655944345887,
+          "pitch": 0.17892863596732056,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "停車場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6336342750905368,
+        "pitch": -0.019850678130795885,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.098438661172521,
+          "pitch": 0.10605101794227778,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.17414648615021378,
+          "pitch": 0.11683146615047235,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "側門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8443640648959772,
+        "pitch": -0.3263166173762446,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.015676364388918174,
+          "pitch": 0.12013040982925816,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0144673304461875,
+          "pitch": 0.05554593154730014,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "進貨車道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0007430870013305,
+        "pitch": 0.012844930634216212,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9682025585577385,
+          "pitch": 0.2133479930879627,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.5906679069457414,
+          "pitch": 0.29794100319955064,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "電子時鐘",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.553307543202184,
+        "pitch": -0.07250271325828095,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6256541892868555,
+          "pitch": 0.3409833718227073,
+          "rotation": 6.283185307179586,
+          "target": "7-"
+        },
+        {
+          "yaw": 2.8671746741842545,
+          "pitch": 0.32817890304233543,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "機車停放區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7748328975575234,
+        "pitch": -0.032630633493266004,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0754103094761582,
+          "pitch": 0.5037523735379708,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.1748420668409274,
+          "pitch": 0.3024418157700488,
+          "rotation": 0.7853981633974483,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "泰山公有市場-2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
