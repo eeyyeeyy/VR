f16842e10a9600415fb46422c21beb5052dba1fd
eeyyeeyy <eeyyeeyy0925@gmail.com>
2024-11-28 11:40:11 +0800

    HOME

---
+++ HOME/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "大門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -0.19034475736011913,
+        "pitch": -0.12228439311839878,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "泰山公有市場-1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
